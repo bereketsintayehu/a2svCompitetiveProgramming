e36b2c4c8c9b9f7899321de2e9581228e825034e
bereketsintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-01-16 17:12:42 +0300

    Delete 1985-find-the-kth-largest-integer-in-the-array directory

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        def sortKey(e):
-            return int(e)
-        nums.sort(reverse=True, key=sortKey)
-        return nums[k-1]
-        
