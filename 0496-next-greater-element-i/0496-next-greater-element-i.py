e39533e39d4b1d77ae50c3bd2410ab3e95102c91
bereketsintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-01-18 08:53:16 -0500

    Time: 101 ms (39.36%), Space: 14.1 MB (95.01%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,14 @@
+from collections import deque
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nextGreater = []
-        for i in range(len(nums1)):
-            nextGreater.append(-1)
+        stack = deque()
+        nextGreater = [-1] * len(nums1)
+        for i in range(len(nums2)):
             
-            for j in range(nums2.index(nums1[i]) + 1, len(nums2)):
-                if nums2[j] > nums1[i]:
-                    nextGreater.pop()
-                    nextGreater.append(nums2[j])
-                    break
+            while len(stack) > 0 and stack[-1] < nums2[i]:
+                nextGreater[nums1.index(stack.pop())] = nums2[i]
+                
+            if nums2[i] in nums1:
+                stack.append(nums2[i])
             
-                    
-        
         return nextGreater
