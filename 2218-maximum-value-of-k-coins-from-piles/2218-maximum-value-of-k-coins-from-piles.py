22347c49c9330ea3ee45d43bb088b3221b16bd20
bereketsintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-04-15 17:25:59 -0400

    Time: 4431 ms (81.99%), Space: 140.6 MB (42.24%) - LeetHub

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
+        @functools.lru_cache(None)
+        def dp(i: int, k: int) -> int:
+            if i == len(piles) or k == 0:
+                return 0
+
+            ans = dp(i + 1, k)  
+            val = 0  
+
+            for j in range(min(len(piles[i]), k)):
+                val += piles[i][j]
+                ans = max(ans, val + dp(i + 1, k - j - 1))
+
+            return ans
+
+        return dp(0, k)
+        
