df692f3498e414cb59fbcf9e443602e247aecd27
bereketsintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-05-20 22:49:55 +0300

    Time: 43 ms (35.17%), Space: 16.4 MB (8.81%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,31 @@
+class Solution:
+  def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    ans = []
+    graph = collections.defaultdict(dict)
+
+    for (A, B), value in zip(equations, values):
+      graph[A][B] = value
+      graph[B][A] = 1 / value
+
+    def devide(A: str, C: str, seen: Set[str]) -> float:
+      if A == C:
+        return 1.0
+
+      seen.add(A)
+
+      for B, value in graph[A].items():
+        if B in seen:
+          continue
+        res = devide(B, C, seen) 
+        if res > 0:  
+          return value * res  
+
+      return -1  
+
+    for A, C in queries:
+      if A not in graph and C not in graph:
+        ans.append(-1.0)
+      else:
+        ans.append(devide(A, C, set()))
+
+    return ans
