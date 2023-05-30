d2759b15c5285fb78eda3110dd2c98922abde285
bereketsintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-05-30 20:19:15 +0300

    Time: 883 ms (11.42%), Space: 33.6 MB (5.81%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,10 +1,12 @@
+import heapq
+
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         ans = 0
         
-        piles.sort(reverse=True)
+        nPiles = heapq.nlargest((len(piles) // 3) * 2, piles)
         
-        for i in range(1, len(piles)-len(piles)//3, 2):
-            ans += piles[i]
+        for i in range(1, len(nPiles), 2):
+            ans += nPiles[i]
         
         return ans
