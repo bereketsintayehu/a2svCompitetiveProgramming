7ad9a999e3a94b7964e52fcf8d384a8b95c3d322
Bereket Sintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-12-06 19:27:52 +0300

    Added solution for Multiply Strings

--- onboarding/multiply-strings.py
+++ onboarding/multiply-strings.py
@@ -1,3 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        inum1, inum2 = 0, 0
+
+        for i in range(len(num1)):
+            inum1 *= 10 
+            inum1 += ord(num1[i]) - 48
+
+
+        for i in range(len(num2)):
+            inum2 *= 10 
+            inum2 += ord(num2[i]) - 48
+        
+        print(inum1, inum2)
+        
+        return str(inum1 * inum2)
