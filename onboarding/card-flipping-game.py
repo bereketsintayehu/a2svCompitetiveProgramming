5692eacf90af4cfe78fc957753b667d493454513
Bereket Sintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-12-13 17:12:46 +0300

    Added solution for Card Flipping Game

---
+++ onboarding/card-flipping-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        s = set(fronts + backs)
+
+        for i in range(min(len(fronts), len(backs))):
+            if fronts[i] == backs[i]:
+                s.discard(fronts[i])
+
+        return min(s) if s else 0
