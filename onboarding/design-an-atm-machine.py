4a4af1e1b1fd881953571383d5e1cb0645b60a3d
Bereket Sintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2023-12-15 20:02:27 +0300

    Added solution for Design an ATM Machine

--- onboarding/design-an-atm-machine.py
+++ onboarding/design-an-atm-machine.py
@@ -2,6 +2,13 @@ class ATM:
 
     def __init__(self):
         self.notes = [0] * 5
+        self.noteValue = {
+            4: 500,
+            3: 200,
+            2: 100,
+            1: 50,
+            0: 20
+        }
 
     def deposit(self, banknotesCount: List[int]) -> None:
         for i in range(5):
@@ -11,51 +18,15 @@ class ATM:
         temp = amount
         used = [0] * 5
 
-        if temp >= 500:
-            n = temp // 500
-            if n > self.notes[4]:
-                temp -= self.notes[4] * 500
-                used[4] = self.notes[4]
+        for i in range(4, -1, -1):
+            n = temp // self.noteValue[i]
+            if n > self.notes[i]:
+                temp -= self.notes[i] * self.noteValue[i]
+                used[i] = self.notes[i]
             else:
-                temp -= n * 500
-                used[4] += n
-        if temp >= 200:
-            n = temp // 200
-            if n > self.notes[3]:
-                temp -= self.notes[3] * 200
-                used[3] = self.notes[3]
-
-            else:
-                temp -= n * 200
-                used[3] += n
-
-        if temp >= 100:
-            n = temp // 100
-            if n > self.notes[2]:
-                temp -= self.notes[2] * 100
-                used[2] = self.notes[2]
-
-            else:
-                temp -= n * 100
-                used[2] += n
-        if temp >= 50:
-            n = temp // 50
-            if n > self.notes[1]:
-                temp -= self.notes[1] * 50
-                used[1] = self.notes[1]
-
-            else:
-                temp -= n * 50
-                used[1] += n
-        if temp >= 20:
-            n = temp // 20
-            if n > self.notes[0]:
-                temp -= self.notes[0] * 20
-                used[0] = self.notes[0]
-
-            else:
-                temp -= n * 20
-                used[0] += n
+                temp -= n * self.noteValue[i]
+                used[i] += n
+        
         if temp:
             return [-1]
         
