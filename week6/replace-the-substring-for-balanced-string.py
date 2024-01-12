a919d30374cb451a8a3f4665cdacf1c6c2e50455
Bereket Sintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2024-01-12 18:14:55 +0300

    Added solution for Replace the Substring for Balanced String

---
+++ week6/replace-the-substring-for-balanced-string.py
@@ -0,0 +1,36 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        n = len(s) // 4
+        map = defaultdict(int)
+        v = set()
+
+        for ch, count in Counter(s).items():
+            if count > n:
+                map[ch] = count - n
+                v.add(ch)
+
+        if len(map) == 0:
+            return 0
+
+        start = end = 0
+        ans = len(s)
+
+        for end in range(len(s)):
+            if s[end] in v:
+                map[s[end]] -= 1
+                if map[s[end]] == 0:
+                    del map[s[end]]
+
+            while all(count <= 0 for count in map.values()):
+                ans = min(ans, end - start + 1)
+                if s[start] in v:
+                    map[s[start]] += 1
+                #     if map[s[start]] <= 0:
+                #         start += 1
+                # else:
+                start += 1
+
+        if len(map) == 0:
+            ans = min(ans, end - start + 1)
+        
+        return ans
