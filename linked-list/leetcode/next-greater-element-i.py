58ed0e29a14c3c222725402f8a69bf04200139c2
Bereket Sintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2024-02-21 18:58:31 +0300

    Add solution for Next Greater Element I

--- linked-list/leetcode/next-greater-element-i.py
+++ linked-list/leetcode/next-greater-element-i.py
@@ -1,12 +1,13 @@
-from collections import deque
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack = deque()
+        stack = []
         n = [-1] * len(nums1)
+        index = {num : i for i, num in enumerate(nums1)}
+
         for i in range(len(nums2)):
             
             while len(stack) > 0 and stack[-1] < nums2[i]:
-                n[nums1.index(stack.pop())] = nums2[i]
+                n[index[stack.pop()]] = nums2[i]
                 
             if nums2[i] in nums1:
                 stack.append(nums2[i])
