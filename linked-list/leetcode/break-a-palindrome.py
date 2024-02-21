a0684f478d755b6420f7d1f9d32a41c139a6cf98
Bereket Sintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2024-02-21 10:51:08 +0300

    Add solution for Break a Palindrome

---
+++ linked-list/leetcode/break-a-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        n = len(palindrome)
+        if n <= 1:
+            return ''
+        
+        s = list(palindrome)
+        
+        for i in range(n // 2):
+            if s[i] != 'a':
+                s[i] = 'a'
+                return ''.join(s)
+        
+        s[-1] = 'b'
+
+        return ''.join(s)
