c27fb9fd584b46d8ab394bbb19a7217121fdaa9f
Bereket Sintayehu <88823044+bereketsintayehu@users.noreply.github.com>
2024-03-04 10:52:45 +0300

    Add solution for Combinations

---
+++ linked-list/leetcode/combinations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        
+        bucket = []
+        def backtrack(i):
+            if len(bucket) == k:
+                ans.append(bucket.copy())
+            
+            if n - i + len(bucket) >= k:
+                for j in range(i + 1, n + 1):
+                    bucket.append(j)
+                    backtrack(j)
+                    bucket.pop()
+
+        backtrack(0)
+        return ans
